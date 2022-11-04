8cd794ce610ee523c48b9dc15f8bb51f41fcd5aa
Vital SOWATH <vitalsowath@gmail.com>
2022-11-04 17:57:08 +0700

    Initial Commit

---
+++ aws-amplify-iso/aws_amplify_isoApp.swift
@@ -0,0 +1,17 @@
+//
+//  aws_amplify_isoApp.swift
+//  aws-amplify-iso
+//
+//  Created by Vital SOWATH on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct aws_amplify_isoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
